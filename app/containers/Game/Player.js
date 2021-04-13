ea2e8f817ae2fd8923890cbfcabaf9c16a1913a5
ashhh90 <andreapalumbosr@hotmail.it>
2021-04-13 15:25:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/containers/Game/Player.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import {css} from 'emotion';
+import { connect } from 'react-redux';
+import {Col, Input, Row, Button } from 'reactstrap';
+import {logEvent} from 'utils/analytics';
+
+import RolePopup from 'components/RolePopup/RolePopup';
+
+export const Player = ({index, player, started, location, role, customLocations, onPlayerChange, localPlayerAmount}) => {
+  const [showRole, setShowRole] = useState(false);
+  const [showedRole, setShowedRole] = useState(false);
+
+  const toggle = () => {
+    if(!showedRole){
+      if (localPlayerAmount > 1){
+        setShowedRole(true); // Only disable showing role if there are other local players as per issue #172
+      }
+    }
+    if(!showRole) logEvent('PLAYER_VIEW_ROLE');
+    setShowRole((prevShowRole) => !prevShowRole);
+  };
+
+  useEffect(() => {
+    setShowedRole(false);
+  }, [started]);
+
+  return (
+    <Row className={styles.player}>
+      <Col>
+        {!started &&
+          <Input type="text" name={`player_${index}`} id={`player_${index}`} placeholder="Player" value={player} onChange={(evt) => onPlayerChange(index, evt.target.value)} />
+        }
+        {!!started &&
+          <Button color="success" disabled={showedRole} outline={showedRole} block onClick={toggle}>{player}</Button>
+        }
+      </Col>
+      <RolePopup isOpen={showRole} toggle={toggle} player={player} location={location} role={role} customLocations={customLocations} />
+    </Row>
+  );
+};
+
+const styles = {
+  player: css({
+    paddingTop: 5,
+    paddingBottom: 5,
+  }),
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const player = state.config.players[ownProps.index];
+  return {
+    player,
+    role: state.game.playersRoles[player],
+    location: state.game.location,
+    customLocations: state.config.customLocations,
+    localPlayerAmount: state.config.players.length,
+  };
+};
+
+export default connect(mapStateToProps)(Player);
